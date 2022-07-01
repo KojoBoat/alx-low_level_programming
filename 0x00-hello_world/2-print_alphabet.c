e34f7c31b2b81b8168dbd531530ff7dcda5ba3ac
KojoBoat <egyirchristian07@gmail.com>
2022-07-01 13:31:26 -0700

    alphabets in lowercase

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: Print alphabets in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ltrs[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for(i = 0; i < 26; i++)
+	{
+		putchar(ltrs[i]);
+	}
+	putchar('\n');
+	return (0);
+}
