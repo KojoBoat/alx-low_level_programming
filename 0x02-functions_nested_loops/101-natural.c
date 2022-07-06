6868cd7dfe0f403448087e324ba1be5f7dfb97ce
KojoBoat <egyirchristian07@gmail.com>
2022-07-06 15:15:57 -0700

    print multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function to print sum of natural
+ * numbers less than 1024
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
