fc11f4803673036c4c30be4343fa9c76f7230a3c
KojoBoat <egyirchristian07@gmail.com>
2022-07-07 13:53:11 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - product of two args
+ * @a: parameter
+ * @b: parameter
+ *
+ * Return: result of product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,4 @@
 int _isupper(int c);
 int _isdigit(int n);
+int mul(int a, int b);
+int _putchar(int c);
