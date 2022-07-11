9a3e15cb4c4ced37a0406036f49e71d287fa4a1e
KojoBoat <egyirchristian07@gmail.com>
2022-07-11 12:56:17 -0700

    0-98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to change the value of an address
+ * @n: parameter
+ *
+ * Return: no return (void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
